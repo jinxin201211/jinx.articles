664223db8790ff81e3829701a02a28982202841a
jinxin201211 <jinxin19950105@outlook.com>
2023-02-14 17:30:57 +0800

    2023年2月14日 17:31:06

--- netty-demo/src/test/java/com/jinx/testnetty/_008TestByteBufferExam.java
+++ netty-demo/src/test/java/com/jinx/testnetty/_008TestByteBufferExam.java
@@ -8,9 +8,24 @@ public class _008TestByteBufferExam {
     public static void main(String[] args) {
         ByteBuffer source = ByteBuffer.allocate(32);
         source.put("Hello world!\nI'm zhangsan\nHo".getBytes());
-        split(source);
+        split1(source);
         source.put("w are you!\n".getBytes());
-        split(source);
+        split1(source);
+    }
+
+    public static void split1(ByteBuffer source) {
+        source.flip();
+        for (int i = 0; i < source.limit(); i++) {
+            if ((char) source.get(i) == '\n') {
+                int length = i + 1 - source.position();
+                ByteBuffer target = ByteBuffer.allocate(length);
+                for (int j = 0; j < length; j++) {
+                    target.put(source.get());
+                }
+                debugAll(target);
+            }
+        }
+        source.compact();
     }
 
     public static void split(ByteBuffer source) {
