bd621b891da7ba1927441f590d8d4af6f2b4b1f2
jinxin201211 <jinxin19950105@outlook.com>
2023-02-24 17:21:31 +0800

    2023年2月24日 17:21:36

--- netty-demo/src/test/java/com/jinx/testnetty/_015MultiThread/MultiThreadClient.java
+++ netty-demo/src/test/java/com/jinx/testnetty/_015MultiThread/MultiThreadClient.java
@@ -3,14 +3,12 @@ package com.jinx.testnetty._015MultiThread;
 import java.io.IOException;
 import java.net.InetSocketAddress;
 import java.nio.channels.SocketChannel;
-import java.nio.charset.StandardCharsets;
 
 public class MultiThreadClient {
     public static void main(String[] args) throws IOException {
         SocketChannel client = SocketChannel.open();
 //        client.bind(new InetSocketAddress(2235));
         client.connect(new InetSocketAddress("127.0.0.1", 2234));
-
 //        client.write(StandardCharsets.UTF_8.encode("Hello Chengdu, it's 2023-02-22 16:19:10"));
         System.in.read();
     }
