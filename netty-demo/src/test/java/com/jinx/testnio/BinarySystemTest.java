b459a3db27da572c3288358a25a5cf26b520c62b
jinxin201211 <jinxin19950105@outlook.com>
2023-09-11 10:00:42 +0800

    BinarySystemTest

---
+++ netty-demo/src/test/java/com/jinx/testnio/BinarySystemTest.java
@@ -0,0 +1,13 @@
+package com.jinx.testnio;
+
+public class BinarySystemTest {
+    public static void main(String[] args) {
+
+        int x = 0x1111;
+        int y = 01111;
+        int z = 0b1111;
+        System.out.println(x);
+        System.out.println(y);
+        System.out.println(z);
+    }
+}
