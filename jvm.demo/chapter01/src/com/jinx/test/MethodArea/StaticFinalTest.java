6cbc73a2271a6b729e5eada61c3e811376a810c4
jinxin201211 <jinxin19950105@outlook.com>
2023-10-30 18:46:51 +0800

    方法区

---
+++ jvm.demo/chapter01/src/com/jinx/test/MethodArea/StaticFinalTest.java
@@ -0,0 +1,9 @@
+package com.jinx.test.MethodArea;
+
+public class StaticFinalTest {
+    public static int number = 0;
+
+    public static final int count = 0;
+
+    public static final String str = "HELLO JVM";
+}
