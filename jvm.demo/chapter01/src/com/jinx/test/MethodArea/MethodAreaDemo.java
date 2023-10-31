95fd325c69acc92cd64368dbd5e851865da644e4
jinxin201211 <jinxin19950105@outlook.com>
2023-10-31 17:31:19 +0800

    方法区

---
+++ jvm.demo/chapter01/src/com/jinx/test/MethodArea/MethodAreaDemo.java
@@ -0,0 +1,11 @@
+package com.jinx.test.MethodArea;
+
+public class MethodAreaDemo {
+    public static void main(String[] args) {
+        int x = 500;
+        int y = 100;
+        double a = x / y;
+        double b = 50;
+        System.out.println(a + b);
+    }
+}
