23f37a714f2df5efed9c9a6ad78ba7bbff61a735
jinxin201211 <jinxin19950105@outlook.com>
2023-11-13 10:46:49 +0800

    方法区

--- jvm.demo/chapter01/src/com/jinx/test/Heap/YoungOldAreaTest.java
+++ jvm.demo/chapter01/src/com/jinx/test/Heap/YoungOldAreaTest.java
@@ -3,6 +3,5 @@ package com.jinx.test.Heap;
 public class YoungOldAreaTest {
     public static void main(String[] args) {
         byte[] buffer = new byte[1024 * 1024 * 20];
-
     }
 }
--- jvm.demo/chapter01/src/com/jinx/test/MethodArea/JavaMethodAreaOOM.java
+++ jvm.demo/chapter01/src/com/jinx/test/MethodArea/JavaMethodAreaOOM.java
@@ -2,9 +2,6 @@ package com.jinx.test.MethodArea;
 
 import net.sf.cglib.proxy.Enhancer;
 import net.sf.cglib.proxy.MethodInterceptor;
-import net.sf.cglib.proxy.MethodProxy;
-
-import java.lang.reflect.Method;
 
 public class JavaMethodAreaOOM {
     public static void main(String[] args) {
