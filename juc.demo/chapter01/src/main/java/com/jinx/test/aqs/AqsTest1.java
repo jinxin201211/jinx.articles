8db949bde1ea233d0dd3d28a55a178c7bcfb36c3
jinxin201211 <jinxin19950105@outlook.com>
2024-02-29 17:30:54 +0800

    synchronized锁升级

---
+++ juc.demo/chapter01/src/main/java/com/jinx/test/aqs/AqsTest1.java
@@ -0,0 +1,10 @@
+package com.jinx.test.aqs;
+
+import java.util.concurrent.locks.ReentrantLock;
+
+public class AqsTest1 {
+    public static void main(String[] args) {
+        ReentrantLock reentrantLock = new ReentrantLock();
+        reentrantLock
+    }
+}
