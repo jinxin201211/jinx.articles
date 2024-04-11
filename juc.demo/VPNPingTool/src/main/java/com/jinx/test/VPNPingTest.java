e6792b33baecf2ad398e23b61dac0c0c3dcd0cc7
jinxin201211 <jinxin19950105@outlook.com>
2024-04-11 15:14:25 +0800

    排除自己的ip

--- juc.demo/VPNPingTool/src/main/java/com/jinx/test/VPNPingTest.java
+++ juc.demo/VPNPingTool/src/main/java/com/jinx/test/VPNPingTest.java
@@ -32,7 +32,7 @@ public class VPNPingTest {
         for (int i = MIN_HOST; i <= MAX_HOST; i++) {
             for (int j = 1; j <= 255; j++) {
                 String ip = "11.16." + i + "." + j;
-                if (!ips.contains(ip)) {
+                if (!ips.contains(ip) && !"11.16.1.218".equals(ip)) {
                     newIps.add(ip);
                 }
             }
