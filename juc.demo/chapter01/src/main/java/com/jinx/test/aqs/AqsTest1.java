ffad28b3dad2e2c33ba003cb2117de4d7efdb9ff
jinxin201211 <jinxin19950105@outlook.com>
2024-05-07 17:12:01 +0800

    AQS

--- juc.demo/chapter01/src/main/java/com/jinx/test/aqs/AqsTest1.java
+++ juc.demo/chapter01/src/main/java/com/jinx/test/aqs/AqsTest1.java
@@ -1,10 +1,29 @@
 package com.jinx.test.aqs;
 
+import com.jinx.tool.TestUtil;
+
 import java.util.concurrent.locks.ReentrantLock;
 
 public class AqsTest1 {
     public static void main(String[] args) {
         ReentrantLock reentrantLock = new ReentrantLock();
-        reentrantLock
+        new Thread(() -> {
+            reentrantLock.lock();
+            try {
+                System.out.println("hello");
+                TestUtil.sleep(1000l);
+            } finally {
+                reentrantLock.unlock();
+            }
+        }).start();
+        new Thread(() -> {
+            reentrantLock.lock();
+            try {
+                System.out.println("wait");
+                TestUtil.sleep(1000l);
+            } finally {
+                reentrantLock.unlock();
+            }
+        }).start();
     }
 }
