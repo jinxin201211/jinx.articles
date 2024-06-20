23f2402a2d9d07a7504dc0d5021882bc1312214d
jinxin201211 <jinxin19950105@outlook.com>
2024-06-20 17:27:45 +0800

    Kotlin

---
+++ kotlin-demo/src/main/kotlin/Tavern.kt
@@ -0,0 +1,15 @@
+class Tavern {
+}
+
+fun main() {
+//    var beverage = readLine()?.capitalize();
+//    beverage = null;
+//    var beverage = readLine()?.let {
+//        if (it.isBlank()) "Butterred Ale" else it.capitalize()
+//    }
+
+//    var beverage = readLine()?.capitalize()?.plus(", large");
+
+    var beverage = readLine();
+    println(beverage)
+}
