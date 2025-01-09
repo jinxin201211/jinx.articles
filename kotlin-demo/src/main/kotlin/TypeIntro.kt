3e3b937406a5be1babb272308f6f60dc8ceb21b7
jinxin201211 <jinxin19950105@outlook.com>
2025-01-09 17:21:39 +0800

    Kotlin

---
+++ kotlin-demo/src/main/kotlin/TypeIntro.kt
@@ -0,0 +1,12 @@
+class TypeIntro {
+}
+
+fun main() {
+    var experiencePoints: Int = 5;
+    experiencePoints += 5;
+    println(experiencePoints)
+
+    val playerName: String = "Estragon";
+//    playerName = "Estar";
+    println(playerName)
+}
