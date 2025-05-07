a5407da3efc809950e91050505b32d8973611f98
jinxin201211 <jinxin19950105@outlook.com>
2025-05-07 23:50:51 +0800

    XJSKP

--- 驾考/XJSKP.sh
+++ 驾考/XJSKP.sh
@@ -23,10 +23,16 @@ i=0;
 while [[ 1+1 ]]
 do
   log "round $i";
-  tap 546 2011;
+  tap 546 2011; #开始游戏、返回
   sleep 1;
-  tap 546 2011;
+  tap 540 1249; #选择词条
+  sleep 1;
+  tap 611 1542; #重新连接
+  sleep 1;
+  tap 594 2068; #继续战斗
+  sleep 1;
+  tap 546 2011; #开始游戏、返回
   i=`expr $i + 1`;
-  sleep 10;
+  sleep 6;
 done
     
