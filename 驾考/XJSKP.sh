107062f5271f8bb4d5edd0a2cae892b441217d29
jinxin201211 <jinxin19950105@outlook.com>
2025-05-07 09:09:27 +0800

    XJSKP

--- 驾考/XJSKP.sh
+++ 驾考/XJSKP.sh
@@ -1,23 +1,32 @@
+function log() {
+  time=$(date "+%Y-%m-%d %H:%M:%S");
+  echo "$time --------> $1";
+}
+
+function tap() {
+  adb shell input tap $1 $2;
+}
+
 echo "start ...";
 
 waiting=20;
 
 while ((waiting>0))
 do
-    echo "请在 $waiting s内打开主界面";
-    waiting=`expr $waiting - 1`;
-    sleep 1;
+  log "请在 $waiting s内打开主界面";
+  waiting=`expr $waiting - 1`;
+  sleep 1;
 done
 
 i=0;
 
 while (true)
 do
-    echo "round $i";
-    adb shell input tap 546 2011;
-    sleep 1;
-    adb shell input tap 546 2011;
-    i=`expr $i + 1`;
-    sleep 10;
+  log "round $i";
+  tap 546 2011;
+  sleep 1;
+  tap 546 2011;
+  i=`expr $i + 1`;
+  sleep 10;
 done
     
