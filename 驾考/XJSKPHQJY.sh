84e72655543379f16497013c98e82b0c1498b89c
jinxin201211 <jinxin19950105@outlook.com>
2025-05-28 23:42:48 +0800

    XJSKPHQJY

--- 驾考/XJSKPHQJY.sh
+++ 驾考/XJSKPHQJY.sh
@@ -18,13 +18,13 @@ do
   log "点击副本邀请";
   tap 1058 1873;
   # tap 1058 1667;
-  # sleep 0.1;
+  sleep 0.01;
   log "接受组队邀请";
   tap 810 734;
-  # sleep 0.1;
+  sleep 0.01;
   log "接受失败时关闭组队邀请窗口";
   tap 1013 549;
-  # sleep 0.1;
+  sleep 0.01;
   i=`expr $i + 1`;
   # sleep 0.2;
 done
