c4bacdba83cc67bcc05a98e6c1e3faa70139ea11
jinxin201211 <jinxin19950105@outlook.com>
2025-06-27 15:15:49 +0800

    XJSKPHQJY2

--- 驾考/XJSKPHQJY2.sh
+++ 驾考/XJSKPHQJY2.sh
@@ -17,6 +17,7 @@ do
   # if (( i % 100 == 0 )); then
   #    adb shell "input tap 86 2300;sleep 3s;input tap 86 2300;input tap 545 2000;input tap 86 2300;input tap 86 2300;input tap 990 2300;"
   # fi
-  adb shell "input tap 1058 1873;input tap 810 734;input tap 1013 549;"
+  # adb shell "input tap 1058 1873;input tap 810 734;input tap 1013 549;"
+  adb shell "input tap 909 1680;input tap 692 727;input tap 1013 549;"
   ((i++))
 done
