c48c50add574e88b0e834b9baed4dc050490c25e
jinxin201211 <jinxin19950105@outlook.com>
2025-09-22 01:02:53 +0800

    XJSKPJYG

--- 驾考/XJSKPJYG.sh
+++ 驾考/XJSKPJYG.sh
@@ -11,11 +11,13 @@ echo "start ...";
 
 round=0;
 
-while ((round<100))
+# while ((round<100))
+while [[ 1+1 ]]
 do
   log "精英关第 $round 轮";
   log "点击向僵尸开炮游戏图标";
-  tap 942 627;
+  # tap 942 627;
+  tap 942 332;
   second=18;
   while ((second>0))
   do
@@ -23,6 +25,19 @@ do
     ((second--))
     sleep 1;
   done
+
+  if (( $round % 10 == 0  && $round != 0)); then
+    log "收取邮件...";
+    adb shell input tap 1016 477;
+    sleep 0.2;
+    adb shell input tap 890 611;
+    sleep 0.2;
+    adb shell input tap 556 1863;
+    sleep 3;
+    adb shell input tap 542 2133;
+    sleep 0.2;
+    adb shell input tap 972 563;
+  fi
   
   log "点击开始游戏";
   tap 547 1924;
